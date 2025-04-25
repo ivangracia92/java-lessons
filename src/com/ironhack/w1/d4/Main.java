8fab51d011b39c4af787679d2cf416823e7dd9d8
Ivan Gracia <ivangracia92@gmail.com>
2025-04-25 11:29:27 +0200

    big decimal

--- src/com/ironhack/w1/d4/Main.java
+++ src/com/ironhack/w1/d4/Main.java
@@ -1,5 +1,8 @@
 package com.ironhack.w1.d4;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -29,7 +32,24 @@ public class Main {
 
         System.out.println(androidPlayer.share());
 
+//      BIG DECIMAL
 
+        System.out.println(1.2 - 1.0);
+
+        BigDecimal bd1 = new BigDecimal("2.0");
+        BigDecimal bd2 = new BigDecimal("2.0");
+
+        System.out.println(bd1.subtract(bd2));
+        System.out.println(bd1.add(bd2));
+        System.out.println(bd1.multiply(bd2));
+        System.out.println(bd1.divide(bd2, RoundingMode.HALF_EVEN));
+
+        System.out.println(bd1.setScale(2, RoundingMode.HALF_EVEN));
+        System.out.println(bd1.compareTo(bd2));
+
+        if (bd1.compareTo(bd2) > 0) System.out.println("bd1 is greater than bd2");
+        else if (bd1.compareTo(bd2) < 0) System.out.println("bd1 is smaller than bd2");
+        else if (bd1.compareTo(bd2) == 0) System.out.println("bd1 is equal do bd2");
 
 
     }
